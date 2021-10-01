a1725fa53a5e31aa1be9e9003bd33b80b3897231
mchrupcala <michael.chrupcala@gmail.com>
2021-10-01 13:07:11 -0400

    Landing page structure and color theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import cameraPhoto from './assets/camera-unsplash.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+          <div className="landing-img">
+              <img src={cameraPhoto} alt='Woman holding a camera surrounded by flowers' />
+          </div>
+
+          <div>
+            <nav>
+              <ul>
+                <li><a href='/'>About</a></li>
+                <li><a href='/'>Login</a></li>
+                <li><a href='/'>Connect Wallet</a></li>
+              </ul>
+            </nav>
+
+            <div className="landing-content">
+              <h1>InstaNFT</h1>
+
+              <br></br><br></br><br></br>
+
+              <form>
+                <input placeholder="Copy/paste your Instagram post URL..."></input>
+                <button type="submit">Mint</button>
+                </form>
+              
+              <div>
+                  <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+                  <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+              </div>
+            </div>
+      </div>
     </div>
   );
 }
