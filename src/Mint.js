9e31837054b72af2c67902faa6380e87a70872f3
Christian Mora <christmo99@gmail.com>
2021-10-11 22:27:06 -0500

    feat: upload file to IPFS

--- src/Mint.js
+++ src/Mint.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 //import { Link } from 'react-router-dom';
+//var request = require('request');
 
 class Mint extends Component {
   state = {
     instagram_code: '#',
     code: '',
     instagram: [],
+    metadata: {},
     redirect_uri: 'https://vigorous-bartik-1b0e50.netlify.app/mint'
   };
 
@@ -47,7 +49,10 @@ class Mint extends Component {
   };
 
   listOfImages = images => {
-    const html = images.map(item => `<img src=${item.media_url} alt=${item.caption} height="400" width="400" />`);
+    const html = images.map(
+      item =>
+        `<img src=${item.media_url} alt=${item.caption} height="400" width="400" />`
+    );
     return html;
   };
 
@@ -58,23 +63,71 @@ class Mint extends Component {
   };
 
   callFunctionImages = async () => {
-    console.log(this.images);
+    this.images = [
+      {
+        media_url:
+          'https://scontent-iad3-2.cdninstagram.com/v/t51.2885-15/1171992_220185431486696_1417365155_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8ae9d6&_nc_ohc=tssZuO0j088AX--a6mc&_nc_ht=scontent-iad3-2.cdninstagram.com&edm=ANQ71j8EAAAA&oh=fc9dbc9c233894897eb37883e4c04580&oe=616A3275',
+        caption: 'This image is a bike',
+        username: 'christmo',
+        id: '123'
+      }
+    ];
+
+    const component = this;
+    let image = this.images[0];
+    let url = image.media_url;
+    let caption = image.caption;
+    let name = image.username + '-' + image.id;
+    console.log(image);
+
+    fetch(url)
+      .then(res => res.blob()) // Gets the response and returns it as a blob
+      .then(blob => {
+        let objectURL = URL.createObjectURL(blob);
+        let myImage = new Image();
+        myImage.src = objectURL;
+        document.getElementById('images').appendChild(myImage);
+        document.blob = blob;
+        component.saveNFT(name, caption, blob);
+      });
   };
 
-  renderImages = async () => {
-    let images = this.state.instagram;
-
-    return (
-      <div className="gallery-view">
-        {images.map((value, index) => {
-          return <img key={index} src={value.media_url} alt={value.caption} style={{width: '400px', height: '400px'}}/>;
-        })}
-      </div>
+  saveNFT = async (name, description, blob) => {
+    const formData = new FormData();
+    formData.append(
+      'file',
+      new File([blob], name + '.jpg', { type: 'image/jpeg' }),
+      name
     );
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        pinata_api_key: 'a6ac6301b2f21db1b27d',
+        pinata_secret_api_key:
+          'dc48cb7b9eaab52c645c63611709d0d97f3a446b532a43158df10d3919b216bd'
+      },
+      body: formData,
+      redirect: 'follow'
+    };
+
+    fetch('https://api.pinata.cloud/pinning/pinFileToIPFS', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        let metadata = {
+          ipfs: 'https://ipfs.io/ipfs/' + data.IpfsHash,
+          gateway: 'https://gateway.pinata.cloud/ipfs/' + data.IpfsHash,
+          caption: description,
+          ...data
+        };
+        console.log(metadata);
+        this.setState({ metadata: metadata });
+      })
+      .catch(error => {
+        console.log('error: ' + error);
+      });
   };
 
   render() {
-    console.log(this.state.instagram);
     return (
       <div>
         <a href={this.state.instagram_code}>Instagram</a>
@@ -82,7 +135,7 @@ class Mint extends Component {
           {this.state.code}
         </p>
         <button onClick={this.callFunction}> Get Images </button>
-        <button onClick={this.callFunctionImages}> Images Console? </button>
+        <button onClick={this.callFunctionImages}> Send 1 to IPFS </button>
         <div id="images" className="gallery-view" />
       </div>
     );
